a1e4398dadd83108a17e7746b3a2a56308e5bf32
tkorays <tkorays@hotmail.com>
2015-04-08 00:37:03 +0800

    add function CreatePermission

--- gum/core/gummanager.php
+++ gum/core/gummanager.php
@@ -7,4 +7,7 @@ class GumManager{
     public function CreateRole($name,$description){
         return $this->db->insert('gum_role',array('name'=>$name,'description'=>$description));
     }
+    public function CreatePermission($type,$name,$description){
+        return $this->db->insert('gum_permission',array('type'=>$type,'name'=>$name,'description'=>$description));
+    }
 }
