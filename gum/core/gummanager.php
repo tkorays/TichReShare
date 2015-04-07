451124ec26cbef9eea15615f151b1556cb4f84fd
tkorays <tkorays@hotmail.com>
2015-04-08 00:54:52 +0800

    ..

--- gum/core/gummanager.php
+++ gum/core/gummanager.php
@@ -11,7 +11,7 @@ class GumManager{
         return $this->db->insert('gum_permission',array('type'=>$type,'name'=>$name,'description'=>$description));
     }
     public function AssignUserRole($uid,$rid){
-        if(!$this->db->get('gum_user','id',array('id'=>$uid))){
+        if(!$this->db->get('gum_user','id',array('uid'=>$uid))){
             return 0;
         }
         if(!$this->db->get('gum_role','id',array('id'=>$rid))){
@@ -22,5 +22,31 @@ class GumManager{
         }else{
             return $this->db->insert('gum_user_role',array('uid'=>$uid,'rid'=>$rid));
         }
+    }
+    public function RemoveUserRole($uid){
+        return $this->db->delete('gum_user_role',array('uid'=>$uid));
+    }
+    public function AssignRolePermission($rid,$pid){
+        if(!$this->db->get('gum_role','id',array('id'=>$rid))){
+            return 0;
+        }
+        if(!$this->db->get('gum_permission','id',array('id'=>$pid))){
+            return 0;
+        }
+        if($this->db->get('gum_role_permission','id',array(
+            'AND'=>array('rid'=>$rid,'pid'=>$pid)
+        ))){
+            return 0;
+        }else{
+            return $this->db->insert('gum_role_permission',array('rid'=>$rid,'pid'=>$pid));
+        }
+    }
+    public function RemoveRolePermission($rid,$pid){
+        return $this->db->delete('gum_role_permission',array(
+            'AND'=>array(
+                'rid'=>$rid,
+                'pid'=>$pid
+            )
+        ));
     }
 }
--- index.php
+++ index.php
@@ -13,5 +13,7 @@ $gum = new Gum('localhost','gum','root','mysql123');
 //$gum->Auth(new usertest(),'auth',null); // 验证需要其他支持
 $manager = $gum->CreateManager();
 //$manager->CreateRole('测试','测试人员');
-//$manager->CreatePermission(1,'delete_file','删除文件');
-echo $manager->AssignUserRole(1,6);
+//$manager->CreatePermission(1,'add_file','添加文件');
+//echo $manager->AssignUserRole(1,7);
+//echo $manager->AssignRolePermission(7,2);
+echo $manager->RemoveRolePermission(7,1);
