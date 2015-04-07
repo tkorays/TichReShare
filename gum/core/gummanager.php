d403e37bbbd554fb01e75d944afbd07ec937b9e1
tkorays <tkorays@hotmail.com>
2015-04-08 00:42:24 +0800

    add function AssignUserPermission

--- gum/core/gummanager.php
+++ gum/core/gummanager.php
@@ -10,4 +10,17 @@ class GumManager{
     public function CreatePermission($type,$name,$description){
         return $this->db->insert('gum_permission',array('type'=>$type,'name'=>$name,'description'=>$description));
     }
+    public function AssignUserRole($uid,$rid){
+        if($this->db->get('gum_user','id',array('id'=>$uid))){
+            return 0;
+        }
+        if($this->db->get('gum_role','id',array('id'=>$rid))){
+            return 0;
+        }
+        if($this->db->get('gum_user_role','id',array('uid'=>$uid))){
+            return $this->db->upadte('gum_user_role',array('uid'=>$uid,'rid'=>$rid));
+        }else{
+            return $this->db->insert('gum_user_role',array('uid'=>$uid,'rid'=>$rid));
+        }
+    }
 }
