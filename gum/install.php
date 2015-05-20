87c4f9a74acab11c216b56910480ff4bbd484ce1
tkorays <tkorays@hotmail.com>
2015-05-20 20:00:10 +0800

    init

--- gum/install.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include_once "gum.php";
-$gum = new Gum('localhost','gum','root','mysql123');
-
-$gum->Setup();
--- schema/role_info.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `user_manager`.`role_info` (
-	`id` INT NOT NULL AUTO_INCREMENT COMMENT '角色id' , 
-	`role_name` VARCHAR(16) NOT NULL COMMENT '角色名字' , 
-	PRIMARY KEY (`id`) 
-) ENGINE = InnoDB DEFAULT CHARSET=utf8;
